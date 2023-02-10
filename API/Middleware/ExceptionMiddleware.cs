fa7f9635f9d2327712baa1cf009afc89855bcd54
Imran Khan <emailofimran1992@gmail.com>
2023-02-11 02:41:23 +0600

    Global exception handler

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,53 @@
+// using Microsoft.AspNetCore.Http;
+
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            this._next = next;
+            this._logger = logger;
+            this._env = env;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "Application/json";
+                context.Response.StatusCode = 500;
+
+                ProblemDetails? response = new ProblemDetails
+                {
+                    Status = 500,
+                    Detail = _env.IsDevelopment() ? ex.StackTrace?.ToString() : null,
+                    Title = ex.Message
+                };
+
+                JsonSerializerOptions? options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                string? json = JsonSerializer.Serialize(response, options);
+                await context.Response.WriteAsync(json);
+
+            }
+        }
+    }
+}
