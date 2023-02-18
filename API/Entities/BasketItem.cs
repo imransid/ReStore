4c5f91b628bdd162ec26265d4a04d31a75f3f9e9
Imran Khan <emailofimran1992@gmail.com>
2023-02-19 02:20:21 +0600

    Basket & BasketItems relations

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
     {
+       
         public int Id { get; set; }
         public int Quantity { get; set; }
 
-        // navigation properties
+        // navigation properties one to one relation
 
         public int ProductId { get; set; }
         public Product? Product {get; set;}
+
+        public int BasketId { get; set; }
+        public Basket basket { get; set; }
     }
 }
