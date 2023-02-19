c2a6b14ec5857db5f1afbf6ffdd4a7c0b2081866
Imran Khan <emailofimran1992@gmail.com>
2023-02-20 05:43:21 +0600

    Table Design

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,38 +1,80 @@
 import { useEffect, useState } from "react";
 import { Basket } from "../../interface/Basket";
 import agent from "../../utils/api";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { RotatingLines } from 'react-loader-spinner'
+import { Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 
-import { RotatingLines } from  'react-loader-spinner'
-import { Typography } from "@mui/material";
+export default function BasketPage() {
+  const [loading, setLoading] = useState(true);
+  const [basket, setBasket] = useState<Basket | null>(null);
+  useEffect(() => {
+    agent.Basket.get()
+      .then((res) => setBasket(res))
+      .catch((err) => {
+        setLoading(false)
+        console.log(err, "Error basket")
+      }).finally(() => {
+        setLoading(false)
+      })
+  }, [setLoading, setBasket])
 
-export default function BasketPage(){
-    const [loading, setLoading] = useState(true);
-    const [basket, setBasket] = useState<Basket | null>(null);
-    useEffect(()=> {
-        agent.Basket.get()
-        .then((res) => setBasket(res))
-        .catch((err) => {
-            setLoading(false)
-            console.log(err , "Error basket")
-        }).finally(() => {
-            setLoading(false)
-        })
-    },[setLoading, setBasket])
+  if (loading) return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
 
-    if(loading) return <RotatingLines
-        strokeColor="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        width="96"
-        visible={true}
+      <Grid item xs={3}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
         />
-    if(!basket) return <Typography variant="h3">Your Basket Is Empty</Typography>
+      </Grid>
+    </Grid>)
 
-    return(
-        <h1>
-            Buyer Id Is : {
-                basket.buyerId
-            }
-        </h1>
-    )
+  if (!basket) return <Typography variant="h3">Your Basket Is Empty</Typography>
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket.items.map(item => (
+            <TableRow
+              key={item.productId}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {item.name}
+              </TableCell>
+              <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">{((item.price / 100) * item.quantity).toFixed(2)}</TableCell>
+              <TableCell align="right">
+                <IconButton color="error">
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
